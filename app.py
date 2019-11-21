a4b656533c7c186d2d8e6f4087ff097d1f9d06af
hrafnara <hrafn02@gmail.com>
2019-11-21 08:58:09 +0000

    fin and fix

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template as rend, session, request
 import hashlib, binascii, os, pymysql,secrets
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 connection = pymysql.connect(host='tsuts.tskoli.is', port=3306, user='1209023850', password='mypassword', database='1209023850_verk7', autocommit=True)
 cursor = connection.cursor()
 #alltaf gaman að fara overboard
@@ -9,21 +10,8 @@ def authorize(user):
     pass
     #todo tengja við localhost, muna að breyta aftur
 
-##stal smá kóða af fólki sem veit betur
-def hash_password(password):
-    #Hash a password for storing.
-    salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
-    pwdhash = hashlib.pbkdf2_hmac('sha512', password.encode('utf-8'), salt, 100000)
-    pwdhash = binascii.hexlify(pwdhash)
-    return (salt + pwdhash).decode('ascii')
- 
 def verify_password(stored_password, provided_password):
-    #Verify a stored password against one provided by user
-    salt = stored_password[:64]
-    stored_password = stored_password[64:]
-    pwdhash = hashlib.pbkdf2_hmac('sha512', provided_password.encode('utf-8'), salt.encode('ascii'), 100000)
-    pwdhash = binascii.hexlify(pwdhash).decode('ascii')
-    return pwdhash == stored_password
+    return provided_password == stored_password
 
 def verify():
     cursor.execute("SELECT * FROM users")
@@ -51,12 +39,12 @@ def addusr():
     print(r)
     cursor.execute("SELECT * FROM users")
     rows = cursor.fetchall()
-    if request.form['username'] in map(lambda x: x[0], rows):
+    if request.form['name'] in map(lambda x: x[0], rows):
         pass
     cursor.execute("""INSERT INTO users (user, pass, nafn) VALUES
     (%s, %s, %s);""" % 
     (connection.escape(r['username']),
-    connection.escape(hash_password(r['password'])),
+    connection.escape(r['password']),
     connection.escape(r['name'])))
     return rend("signup.html",code=0)
 
@@ -67,13 +55,12 @@ def login_page():
 @app.route("/login/process",methods=['POST'])
 def login():
     r=request.form
-    cursor.execute("""select * from users where user=%s;""" % (
-        connection.escape(r['username'])))
+    cursor.execute("""select * from users where user="%s";""" % r["username"])
     rows = cursor.fetchall()
     if rows!=():
         user = rows[0]
-        if verify_password(user[2],r["password"]):
-            session["user"] = {"uname":user[0],"nafn":user[1],"pass":user[2]}
+        if verify_password(user[1],r["password"]):
+            session["user"] = {"username":user[0],"nafn":user[2],"pass":user[1]}
             print(session["user"])
         else: 
             return rend("login.html", code=1)
@@ -84,7 +71,7 @@ def login():
 @app.route("/account")
 def account():
     if "user" in session.keys():
-        if session["user"]["uname"] != None:
+        if session["user"]["username"] != None:
             return rend("account.html", user = session["user"])
     return index()
 @app.route("/logout")
